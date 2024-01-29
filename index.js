30d61bdcb3ac0cd92c82a8f2d52806c51f512ea8
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-29 13:56:12 +0500

    Project Modules

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var service_1 = require("./service");
+(0, service_1.func)();
---
+++ service.ts
@@ -0,0 +1,4 @@
+export let func = ():void=>{
+    console.log("Hello Ahsen")
+}
+
